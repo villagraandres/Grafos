1300f846dfce8a7bd67968c107c134c2ba332877
villagraandres <villagraandres00@gmail.com>
2025-10-31 12:54:06 -0600

    ciudad y pais

---
+++ Grafos/Pages/Ciudad.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Grafos.Data;
+using Grafos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+namespace Grafos.Pages
+{
+    public class CiudadModel:PageModel
+    {
+
+        private readonly AppDbContext _context;
+        public CiudadModel(AppDbContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+
+        
+
+        public Ciudad NuevaCiudad {  get; set; }=new Ciudad();
+        
+
+        public async Task OnGetAsync()
+        {
+            var listaPaises=await _context.Ciudades.ToListAsync();
+
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            _context.Ciudades.Add(NuevaCiudad);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+            
+        }
+    }
+}
---
+++ Grafos/Pages/Pais.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Grafos.Data;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Grafos.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Grafos.Pages
+{
+    public class PaisModel : PageModel
+    {
+
+
+        private readonly AppDbContext _context;
+
+        public PaisModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+
+        public Pais NuevoPais { get; set; } = new Pais();
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            _context.Paises.Add(NuevoPais);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("./Index");
+
+        }
+    }
+}
